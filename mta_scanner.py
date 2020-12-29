5cf1218c92e89f8d9d5aae81a3d5b332754cd6b0
Muhammad <55559182+xirmuhammadxir@users.noreply.github.com>
2020-12-29 12:15:55 +0300

    Update mta_scanner.py

--- mta_scanner.py
+++ mta_scanner.py
@@ -7,3 +7,7 @@ def data():
     print("System information: ")
     print('System :', platform.system())  # Отображение названия системы / ОС
     print('Distribution :', platform.platform())  # Показать название ОС, версию и кодовое имя
+    print(platform.architecture())  # Показать архитектуру машины
+    print('Node :', platform.node())  # Показать имя компьютера в сети
+    print('Machine :', platform.machine())  # Показать тип машины
+    print('Processor :', platform.processor())  # Показать имя процессора
