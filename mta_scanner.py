509f4984e2d2270478f475cc77e5462a8c179356
holly255 <serikov.timofei1999@gmail.com>
2020-12-29 16:37:05 +0700

    Update mta_scanner.py

--- mta_scanner.py
+++ mta_scanner.py
@@ -11,3 +11,11 @@ def data():
     print('Node :', platform.node())  # Показать имя компьютера в сети
     print('Machine :', platform.machine())  # Показать тип машины
     print('Processor :', platform.processor())  # Показать имя процессора
+
+
+def installed():
+    dpkg_list = shlex.split("dpkg-query -Wf '${Installed-Size}\t${Package}\n'")
+    process = subprocess.Popen(dpkg_list, stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print("List installed application: ")
+    print(output.decode('UTF-8'))
